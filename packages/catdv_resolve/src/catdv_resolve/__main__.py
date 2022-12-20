6e6c4e18f54ca70fdc0d33b4135379b6c846e6eb
Joseph Clack <joe@joeclack.me.uk>
2022-12-20 16:40:42 +0000

    begin to implement the install module

--- packages/catdv_resolve/src/catdv_resolve/__main__.py
+++ packages/catdv_resolve/src/catdv_resolve/__main__.py
@@ -1,9 +1,68 @@
+from __future__ import annotations
 import argparse
 import sys
+from pathlib import Path
+import logging
+from os import symlink as os_symlink
+from enum import Enum, auto
 
 
-def install_plugin_symlink(args: argparse.Namespace) -> None:
-    print("I would try to make a symlink now!")
+class Platform(Enum):
+    Unknown = auto()
+    OSX = auto()
+    Linux = auto()
+    Windows = auto()
+
+    @classmethod
+    def determine(cls) -> Platform:
+        import platform
+        system_name = platform.system()
+        if system_name == "Darwin":
+            return cls.OSX
+        elif system_name == "Linux":
+            return cls.Linux
+        elif system_name == "Windows":
+            return cls.Windows
+        else:
+            return cls.Unknown
+
+
+class Installer:
+    def __init__(self) -> None:
+        self.system_platform = Platform.determine()
+
+    def get_linux_resolve_dir(self) -> Path:
+        if self.system_platform != Platform.Linux:
+            raise OSError
+
+        opt_path = Path("opt", "resolve").resolve()
+        home_path = Path("home", "resolve").resolve()
+
+        if home_path.is_dir():
+            return home_path
+        else:
+            return opt_path
+
+    def get_resolve_system_scripts_directory(self) -> Path:
+        if self.system_platform == Platform.OSX:
+            return Path("Library", "Application Support", "Blackmagic Design", "DaVinci Resolve", "Fusion", "Scripts")
+        elif self.system_platform == Platform.Linux:
+            return Path(self.get_linux_resolve_dir(), "Fusion", "Scripts")
+        elif self.system_platform == Platform.Windows:
+            from os import getenv as osgetenv
+            return Path(osgetenv("PROGRAMDATA"), "Blackmagic Design", "DaVinci Resolve", "Fusion", "Scripts")
+        else:
+            raise NotImplementedError("Unsupported platform; can't find DaVinci Resolve's Scripts directory.")
+
+    @staticmethod
+    def get_package_directory():
+        return Path(__file__).resolve().parent
+
+    def install_plugin_symlink(self, args: argparse.Namespace) -> None:
+        symlink_destination = Path(self.get_resolve_system_scripts_directory(), "Utility", "CatDV-Resolve.py")
+        symlink_destination.unlink(missing_ok=True)
+
+        os_symlink(Path(__file__, "source", "bootstrap.py"), symlink_destination)
 
 
 class ParserThatGivesUsageOnError(argparse.ArgumentParser):
@@ -11,6 +70,9 @@ class ParserThatGivesUsageOnError(argparse.ArgumentParser):
         sys.stderr.write(f"Error: {message}\n")
         self.print_help()
         sys.exit(2)
+
+
+installer_instance = Installer()
 
 
 parser = ParserThatGivesUsageOnError(description="Use the CatDV Resolve Plugin command-line tool.")
@@ -21,10 +83,16 @@ subparsers = parser.add_subparsers(
 )
 
 install_parser = subparsers.add_parser("install", help="install the plugin")
-install_parser.set_defaults(func=install_plugin_symlink)
+install_parser.set_defaults(func=installer_instance.install_plugin_symlink)
 
 args = parser.parse_args()
 try:
-    args.func(args)
+    args.func
 except AttributeError:
     parser.error("No command specified")
+
+try:
+    args.func(args)
+except Exception as error:
+    logging.fatal(error)
+    parser.error("Unexpected error occurred.")
